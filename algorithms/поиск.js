6c5d1cc492fa7ce70d25dc944e0af172da1d8b48
ellankz <enelipa@gmail.com>
2020-11-06 17:29:39 +0200

    fix: remove extra code

--- algorithms/поиск.js
+++ algorithms/поиск.js
@@ -32,20 +32,3 @@ let BinarySearch = (list,val)=>{
       return -1
   }
 }
-
-
-// Поиск подстроки
-function SubstringSearch(sub, str)    // sub - искомая подстрока
-{                                     // str - строка, в которой ищем
-    var i, j, n = sub.length,
-        N = str.length - n + 1;
-    
-    for (i = 0; i < N; i++)
-    {  j = 0;
-       while (j < n && sub.charAt(j) === str.charAt(i+j)) j++;
-       if (j === n) return i;
-    }                                // На выходе индекс 1-го символа подстроки.
-                                     // Если искомой подстроки нет в строке, то -1.
-    return -1;                       // Например,
-}                                    // SubstringSearch('ips', 'Lorem ipsum') = 6,
-                                     // SubstringSearch('dolor', 'Lorem ipsum') = -1.
